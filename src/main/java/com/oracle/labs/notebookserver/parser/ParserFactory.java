f170425f1d99246153f2064f7e260a91412823cf
Marouane EL HALLAOUI <marouane.elhallaoui@nestle.com>
2019-06-25 19:48:02 +0100

    remove camelcase naming format from abstract playload input class and related refactoring

--- src/main/java/com/oracle/labs/notebookserver/parser/ParserFactory.java
+++ src/main/java/com/oracle/labs/notebookserver/parser/ParserFactory.java
@@ -4,13 +4,13 @@ package com.oracle.labs.notebookserver.parser;
  * Created by EL HALLAOUI MAROUANE on 6/23/2019.
  */
 public class ParserFactory {
-    private static AbstractPlayLoadParser parserInstace;
+    private static AbstractPlayloadParser parserInstace;
 
     static {
         parserInstace = new PlayLoadParserImplementation();
     }
 
-    public static AbstractPlayLoadParser getParser() {
+    public static AbstractPlayloadParser getParser() {
         return parserInstace;
     }
 }
